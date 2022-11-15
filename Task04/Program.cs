08f3492ef2488b90876e00f6d1c08e858f83882f
Monte_Kristo_VM <monte_kristo2003@mail.ru>
2022-11-15 21:03:12 +0300

    решено

---
+++ Task04/Program.cs
@@ -0,0 +1,19 @@
+﻿// 1. Принимаем на вход 3 числа
+// 2. Выдаем максимальное
+
+Console.WriteLine("Введите первое целое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе целое число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье целое число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if (max < num2)  max = num2;
+if (max < num3)  max = num3;
+
+Console.WriteLine($"Максимальное число {max}");
+
