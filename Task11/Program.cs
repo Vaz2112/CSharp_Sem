5cf91d55580d7527f3bec4b1ac692a3195b5d1c2
Monte_Kristo_VM <monte_kristo2003@mail.ru>
2022-11-17 20:52:25 +0300

    Task 11 completed

---
+++ Task11/Program.cs
@@ -0,0 +1,21 @@
+﻿// 1. Выводит случайное трехзначное число
+// 2. Удаляет вторую цифру этого числа
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Cлучайное трехзначное число --> {number}");
+
+int DelSecondDigit(int num)
+{
+    int firstDigit = num / 100;
+    int thirdDigit = num % 10;
+
+    int result = firstDigit * 10 + thirdDigit;
+    return result;
+}
+
+int res=DelSecondDigit(number);
+Console.WriteLine($"Искомое число --> {res}");
