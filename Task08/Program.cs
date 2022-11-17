2faaddb0c5af851992783c4461d385bb693bba64
Monte_Kristo_VM <monte_kristo2003@mail.ru>
2022-11-17 14:48:17 +0300

    Task 08 correction

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -5,9 +5,21 @@
 Console.WriteLine("Введите целое число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 int count = 2;
+
+/* Не совсем верный вариант, в случае ввода нечетного числа,
+выводит последнее четное с запятой после него (11 -> 10,)
+
 while (count <= number)
 {
     if (count != number && count % 2 == 0) Console.Write(count + ", ");
+    else if (count % 2 == 0) Console.Write(count);
+    count++;
+} */
+
+//Вариант, подсказанный преподавателем, работает для всего корректно
+while (count <= number)
+{
+    if (count < number - 1 && count % 2 == 0) Console.Write(count + ", ");
     else if (count % 2 == 0) Console.Write(count);
     count++;
 }
