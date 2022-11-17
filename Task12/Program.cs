1d35bb6bb223334c425e21b1e40cb55a9eb4791a
Monte_Kristo_VM <monte_kristo2003@mail.ru>
2022-11-17 21:07:57 +0300

    Task 12 completed

---
+++ Task12/Program.cs
@@ -0,0 +1,22 @@
+﻿// 1. Принимаем на вход два числа
+// 2. Является ли первое число, кратным второму
+
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+Console.WriteLine("Введите 2 числа:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int Multiplicity(int number1, int number2)
+{
+    return number1 % number2;
+}
+
+int res = Multiplicity(num1, num2);
+if (res == 0)
+{
+    Console.WriteLine("Кратно");
+
+}
+else Console.WriteLine($"Не кратно, остаток {res}");
