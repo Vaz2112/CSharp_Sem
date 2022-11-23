04057a3ee630101b8322dc2fa49f08f7efe98245
Monte_Kristo_VM <monte_kristo2003@mail.ru>
2022-11-23 21:19:22 +0300

    Task 24 completed

---
+++ Task24/Program.cs
@@ -0,0 +1,26 @@
+﻿// 1. принимает на вход число А
+// 2. выдает сумму чисел от 1 до А
+
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumNumbers(int num)
+{
+    int sum =0;
+    for(int i=1; i<=num; i++)
+    {
+        sum+=i;
+    }
+    return sum;
+}
+
+if (number >0)
+{
+int sumNumbers= SumNumbers(number);
+Console.WriteLine($"Сумма чисел от 1 до {number} = {sumNumbers}");
+}
+else Console.WriteLine("Введите натуральное число!");
