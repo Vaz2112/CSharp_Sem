73448333d559abfb73fb183ab60d6fa716e158df
Monte_Kristo_VM <monte_kristo2003@mail.ru>
2022-11-24 19:47:57 +0300

    Task 29 completed

---
+++ Task29/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу, которая 
+// 1.задаёт массив из 8 элементов и 
+// 2.выводит их на экран.
+
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+int[] array = new int[8];
+
+Console.WriteLine("Введите целое число - нижнюю границу диапазона случайных чисел: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое число - верхнюю границу диапазона случайных чисел: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+void FillArray(int[] arr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.Write($"{arr[arr.Length - 1]}]");
+}
+
+if (min <= max)
+{
+    FillArray(array);
+    PrintArray(array);
+}
+else
+{
+    Console.WriteLine("Неверный диапазон!");
+}
