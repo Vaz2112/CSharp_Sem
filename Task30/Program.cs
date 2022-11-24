9524499f9c36d4323a72fcf8c42bacbb2ba2e916
Monte_Kristo_VM <monte_kristo2003@mail.ru>
2022-11-24 18:34:19 +0300

    Task 30 completed

---
+++ Task30/Program.cs
@@ -0,0 +1,35 @@
+﻿int[] array = new int[8];
+
+// int[] FillArray(int[] arr)
+// {
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         arr[i] = new Random().Next(0, 2);
+//     }
+//     return arr;
+// }
+
+void FillArray(int[] arr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(0, 2);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length-1; i++)
+    {
+        Console.Write(arr[i]+", ");
+    }
+    Console.Write($"{arr[arr.Length-1]}]");
+}
+
+//int[] resArray = FillArray(array);
+//PrintArray(resArray);
+
+FillArray(array);
+PrintArray(array);
