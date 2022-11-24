379442bb780403a287cf2179c44b1464123e127d
Monte_Kristo_VM <monte_kristo2003@mail.ru>
2022-11-24 19:02:41 +0300

    Task 25 completed

---
+++ Task25/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите цикл, который 
+// 1.принимает на вход два числа (A и B) и 
+// 2.возводит число A в натуральную степень B.
+
+// 3, 5 -> 243
+// 2, 4 -> 16
+
+Console.WriteLine("Введите любое число, которое нужно возвести в степень: ");
+double number = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите степень (натуральное число): ");
+int degree = Convert.ToInt32(Console.ReadLine());
+
+double Exponentiation(double num, int degr)
+{
+    double res = num;
+    for(int i=1; i<degr; i++)
+    {
+        res=res*num;
+    }
+    return res;
+}
+
+if (degree >0)
+{
+double exponentiation= Exponentiation(number, degree);
+Console.WriteLine($"Число {number} в степени {degree} = {exponentiation}");
+}
+else Console.WriteLine("Введите натуральную степень!");
