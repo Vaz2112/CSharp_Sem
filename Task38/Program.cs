ea57ef431b0c2de6e570992102c5b327cc11eb23
Monte_Kristo_VM <monte_kristo2003@mail.ru>
2022-11-28 22:41:16 +0300

    Task 38 completed

---
+++ Task38/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задайте 
+// 1.массив вещественных чисел. 
+// 2. Найдите разницу между максимальным и минимальным элементами массива.
+
+// [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+
+double[] CreateArrayRndDouble(int size, int min, int max)
+{
+    double[] array = new double[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        double num = rnd.NextDouble() * (max - min) + min;
+        array[i] = Math.Round(num, 1);
+    }
+    return array;
+}
+
+void PrintArray(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+double DifferenceMaxMin(double[] array)
+{
+    double min = array[0];
+    double max = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (max < array[i]) max = array[i];
+        if (min > array[i]) min = array[i];
+    }
+    return max - min;
+}
+
+double[] arr = CreateArrayRndDouble(10, 0, 100);
+
+PrintArray(arr);
+
+double differenceMaxMin = DifferenceMaxMin(arr);
+
+Console.WriteLine($"Разницу между максимальным и минимальным элементами массива = {differenceMaxMin}");
