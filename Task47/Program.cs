3dab5405f8cd6ec904c9b79f9067ed34ce880e73
Monte_Kristo_VM <monte_kristo2003@mail.ru>
2022-12-03 11:35:15 +0300

    Task 47 completed

---
+++ Task47/Program.cs
@@ -0,0 +1,40 @@
+﻿/*  Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9 */
+
+double[,] CreateMatrixRndDoubleRound(int rows, int columns, int min, int max, int round)
+{
+    double[,] matrix = new double[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.NextDouble() * (max - min) + min;
+            matrix[i, j] = Math.Round(matrix[i, j], round);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrixDouble(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5}, ");
+            else Console.Write($"{matrix[i, j],5} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+double[,] array2DdoubleRound = CreateMatrixRndDoubleRound(3, 4, -10, 10, 2);
+PrintMatrixDouble(array2DdoubleRound);
