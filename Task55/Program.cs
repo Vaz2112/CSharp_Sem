f370e4096ff4518913fe575124d5dd2b49baca7e
Monte_Kristo_VM <monte_kristo2003@mail.ru>
2022-12-07 10:02:04 +0300

    Task 55 completed

---
+++ Task55/Program.cs
@@ -0,0 +1,57 @@
+﻿/* Задайте двумерный массив. Напишите программу,
+которая заменяет строки на столбцы. В случае, если это
+невозможно, программа должна вывести сообщение для
+пользователя. */
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+void ReplaceRowsToColumnsMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = i; j < matrix.GetLength(1); j++)
+        {
+            int temp = matrix[j, i];
+            matrix[j, i] = matrix[i, j];
+            matrix[i, j] = temp;
+        }
+    }
+}
+
+int[,] array2D = CreateMatrixRndInt(5, 5, -10, 10);
+PrintMatrix(array2D);
+Console.WriteLine();
+if (array2D.GetLength(0) != array2D.GetLength(1))
+    Console.WriteLine("Не возможно перевернуть неквадратную матрицу!");
+else
+{
+    ReplaceRowsToColumnsMatrix(array2D);
+    PrintMatrix(array2D);
+}
