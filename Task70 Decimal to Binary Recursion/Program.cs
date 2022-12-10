f8d7e73c3cbcfed69e94fc1419f08b2bf43250c2
Monte_Kristo_VM <monte_kristo2003@mail.ru>
2022-12-10 13:13:52 +0300

    Converting decimal to binary recursion

---
+++ Task70 Decimal to Binary Recursion/Program.cs
@@ -0,0 +1,15 @@
+﻿//Преобразование десятичного числа в двоичное использую рекурсию
+
+Console.WriteLine("Введите десятичное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+DecimalToBinary(number);
+
+void DecimalToBinary(int num)
+{
+    if (num > 0)
+    {
+        DecimalToBinary(num/2);
+        Console.Write($"{num%2}");
+    }
+}
