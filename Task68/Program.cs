b5bfe175a97cce4a8a722dc9a291816a74195617
Monte_Kristo_VM <monte_kristo2003@mail.ru>
2022-12-11 20:50:19 +0300

    Task 68 correction

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -13,10 +13,10 @@ int AckermanFunctionRecursive(int m, int n)
 {
     if (m == 0)
         return n + 1;
-    else if (n == 0)
+    if (n == 0)
         return AckermanFunctionRecursive(m - 1, 1);
-    else
-        return (AckermanFunctionRecursive(m - 1, AckermanFunctionRecursive(m, n - 1)));
+
+    return (AckermanFunctionRecursive(m - 1, AckermanFunctionRecursive(m, n - 1)));
 }
 
 if (m >= 0 && n >= 0)
